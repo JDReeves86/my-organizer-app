4faca6e6634909a6847ae3d3ff9a8f904f7244f0
JDReeves86 <Reeves.Jacobd@gmail.com>
2023-03-01 13:31:07 -0800

    added bold and italic buttons - needs formatting

--- client/src/components/DraftComponents/MyEditor.js
+++ client/src/components/DraftComponents/MyEditor.js
@@ -1,29 +1,43 @@
 import React, { useState } from "react";
 import { Editor, EditorState, RichUtils } from "draft-js";
-import 'draft-js/dist/Draft.css';
+import "draft-js/dist/Draft.css";
 
 const initialState = EditorState.createEmpty();
 
 function MyEditor() {
   const [editorState, setEditorState] = useState(initialState);
-  console.log(editorState)
+  const [boldToggle, setBoldToggle] = useState(false);
+  console.log(boldToggle);
 
   const handleKeyCommand = (command, editorState) => {
     const newState = RichUtils.handleKeyCommand(editorState, command);
     if (newState) {
-      setEditorState(newState)
-      return 'handled'
+      setEditorState(newState);
+      return "handled";
     }
-    return 'not-handled'
-  }
+    return "not-handled";
+  };
+
+  const onBoldClick = () => {
+    setEditorState(RichUtils.toggleInlineStyle(editorState, "BOLD"));
+  };
+
+  const onItalicClick = () => {
+    setEditorState(RichUtils.toggleInlineStyle(editorState, "ITALIC"));
+  };
 
   return (
+    <>
+      <button onClick={onBoldClick.bind(editorState)}>Bold</button>
+
+      <button onClick={onItalicClick.bind(editorState)}>italic</button>
       <Editor
         editorState={editorState}
         onChange={setEditorState}
         handleKeyCommand={handleKeyCommand}
         placeholder="hello type something"
       />
+    </>
   );
 }
 
