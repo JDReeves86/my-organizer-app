b17f714260bc17b17b73a3044d0dab38c62cc9c1
JDReeves86 <Reeves.Jacobd@gmail.com>
2023-03-07 13:06:14 -0800

    populating saved notes into editor

--- client/src/components/DraftComponents/MyEditor.js
+++ client/src/components/DraftComponents/MyEditor.js
@@ -16,15 +16,21 @@ import { escapeQuotesforJSON } from "../../utils/helpers";
 import "draft-js/dist/Draft.css";
 
 function MyEditor({ activeNote }) {
-  const [editorState, setEditorState] = useState({});
+  const [content, setContent] = useState(EditorState.createEmpty());
+
+  console.log(activeNote);
 
   useEffect(() => {
-    activeNote === undefined
-    ? (setEditorState({ editorState: EditorState.createEmpty() }))
-    : (setEditorState({ editorState: EditorState.createWithContent(
-        convertFromRaw(activeNote)) }
-      ));
-  }, [editorState])
+    activeNote !== undefined
+      ? setContent(EditorState.createWithContent(
+            convertFromRaw(activeNote)
+          ),
+        )
+      : setContent(EditorState.createEmpty());
+  }, [activeNote]);
+
+  console.log(content.getCurrentContent());
+  console.log(content)
 
   const [title, setTitle] = useState("Untitled Note");
 
@@ -32,36 +38,34 @@ function MyEditor({ activeNote }) {
 
   if (error) return <ErrorModal message={error.message} activate={true} />;
 
-  if (!editorState) {
-    return (
-      <h1>Loading</h1>
-    )
+  if (!content) {
+    return <h1>Loading</h1>;
   }
 
-  const handleKeyCommand = (command, editorState) => {
-    const newState = RichUtils.handleKeyCommand(editorState, command);
+  const handleKeyCommand = (command, content) => {
+    const newState = RichUtils.handleKeyCommand(content, command);
     if (newState) {
-      setEditorState(newState);
+      setContent(newState);
       return "handled";
     }
     return "not-handled";
   };
 
   const onBoldClick = () => {
-    setEditorState(RichUtils.toggleInlineStyle(editorState, "BOLD"));
+    setContent(RichUtils.toggleInlineStyle(content, "BOLD"));
   };
 
   const onItalicClick = () => {
-    setEditorState(RichUtils.toggleInlineStyle(editorState, "ITALIC"));
+    setContent(RichUtils.toggleInlineStyle(content, "ITALIC"));
   };
 
   const onUnderlineClick = () => {
-    setEditorState(RichUtils.toggleInlineStyle(editorState, "UNDERLINE"));
+    setContent(RichUtils.toggleInlineStyle(content, "UNDERLINE"));
   };
 
   const handleSubmit = async () => {
     try {
-      const contentState = editorState.getCurrentContent();
+      const contentState = content.getCurrentContent();
       const stringifiedContent = JSON.stringify(convertToRaw(contentState));
       const escapedContent = escapeQuotesforJSON(stringifiedContent);
       const noteData = {
@@ -91,30 +95,29 @@ function MyEditor({ activeNote }) {
       <button
         className="myEditorBtn has-background-grey-lighter"
         id="boldBtn"
-        onClick={onBoldClick.bind(editorState)}
+        onClick={onBoldClick.bind(content)}
       >
         B
       </button>
       <button
         className="myEditorBtn has-background-grey-lighter"
         id="underlineBtn"
-        onClick={onUnderlineClick.bind(editorState)}
+        onClick={onUnderlineClick.bind(content)}
       >
         U
       </button>
       <button
         className="myEditorBtn has-background-grey-lighter"
         id="italicBtn"
-        onClick={onItalicClick.bind(editorState)}
+        onClick={onItalicClick.bind(content)}
       >
         I
       </button>
       <Editor
         className="myNoteEditor"
-        editorState={editorState}
-        onChange={setEditorState}
+        editorState={content}
+        onChange={setContent}
         handleKeyCommand={handleKeyCommand}
-        
       />
       <Button attr="is-success" action={handleSubmit}>
         Save
